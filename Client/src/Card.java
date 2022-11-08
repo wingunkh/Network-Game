4ca3a60b61f37e9b429bea8ff28d4fba72367d21
geonhee <garnett0122@hansung.ac.kr>
2022-11-08 19:30:17 +0900

    Card 클래스 추가

---
+++ Client/src/Card.java
@@ -0,0 +1,52 @@
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+class Card {
+	private int cardNum;
+	private String cardSrc;
+	private String backSrc = "src/cards/Back.png";
+	private JLabel card;
+	private ImageIcon cardIcon;
+	
+	public void setCardNum(int cardNum) {
+		this.cardNum = cardNum;
+	}
+	public void setCardSrc(int srcNum) {
+		this.cardSrc = "src/cards/" + srcNum + ".png";
+	}
+	public void setCardIcon(String src) {
+		this.cardIcon = new ImageIcon(src);
+	}
+	public void setCard() {
+		this.card = new JLabel(Integer.toString(cardNum));
+		this.card.setIcon(this.cardIcon);
+	}
+	public int getCardNum() {
+		return this.cardNum;
+	}
+	
+	public String getCardSrc() {
+		return this.cardSrc;
+	}
+	public String getBackSrc() {
+		return this.backSrc;
+	}
+	public JLabel getCard() {
+		return this.card;
+	}
+	
+	public Card(int num) {
+		setCardNum(num);
+		setCardIcon(backSrc);
+		setCard();
+	}
+	
+	/* JavaObjectClientView ¿¹½Ã
+	 * Card[] myCards = new Card[3];
+	 * for (int i = 0; i < 3; i++) {
+	 * myCards[i] = new Card(i);
+	 * }
+	 * =>
+	 * myCards[0] => cardNum = 0, cardSrc = "";
+	 */
+}
